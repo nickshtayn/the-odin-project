0f1ff68d4ec84da8a5cdaa50109d9929e1e92822
Nick Shtayn <nickolasshtayn@protonmail.com>
2024-03-05 13:23:06 -0600

    Refine psuedocode for clarity and detail in logic

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -1,16 +1,31 @@
 function getComputerChoice() {
-    // return 'Rock', 'Paper', or 'Scissors' randomly
+    // let choices = array ["rock", "paper", "scissors"];
+    // return choices.random();
 }
 
 function playRound(playerSelection, computerSelection) {
-    // plays a round, if player wins return "You Win! X beats Y" if player loses return "You Lose! X beats Y"
-}
+    // let playerSelection = playerSelection.toLowerCase()
+    // let computerSelection = getComputerChoice()
+
+    // if playerSelection == rock && computerSelection == rock return "its a tie" + "tie" result
+    // if playerSelection == rock && computerSelection == paper return "you lost" + "computer" result
+    // if playerSelection == rock && computerSelection == scissors reutnr "you win" + "player" result
+
+    // if playerSelection == paper && computerSelection == rock return "you win" + "player" result
+    // if playerSelection == paper && computerSelection == paper return "its a tie" + "tie" result
+    // if playerSelection == paper && computerSelection == scissors return "you lose" + "computer" result
+
+    // if playerSelection == scissors && computerSelection == rock return "you lose" + "computer" result
+    // if playerSelection == scissors && computerSelection == paper return "you win" + "player" result
+    // if playerSelection == scissors && computerSelection == scissors return "its a tie" + "tie" result
+    }
 
 function playGame() {
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
+    // let playerScore = 0
+    // let computerScore = 0
+    // playRound() x5
+    // if result "player" return playerScore =+1
+    // if result "computer" return computerScore =+1
+    // if result "tie" return playerScore = playerScore && computerScore = computerScore
     // return winner of playGame() 
 }
