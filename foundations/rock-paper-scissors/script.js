a574569a663cf3a040dec698d419927d04230c90
Nick Shtayn <nickolasshtayn@protonmail.com>
2024-03-05 12:34:16 -0600

    Add pseudocode for instructions

---
+++ foundations/rock-paper-scissors/script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    // return 'Rock', 'Paper', or 'Scissors' randomly
+}
+
+function playRound(playerSelection, computerSelection) {
+    // plays a round, if player wins return "You Win! X beats Y" if player loses return "You Lose! X beats Y"
+}
+
+function playGame() {
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    // return winner of playGame() 
+}
