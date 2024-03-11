985293916add1fdc65e8dabeef1cd74e21c3ff2c
Nick Shtayn <nickolasshtayn@protonmail.com>
2024-03-11 10:54:07 -0500

    Complete project

--- foundations/etch-a-sketch/script.js
+++ foundations/etch-a-sketch/script.js
@@ -1,11 +1,35 @@
-
-const container = document.getElementById("container");
-
-for (let row = 0; row < 16; row++) {
-    const gridRow = document.createElement("div");
-    container.appendChild(gridRow);
-    for (let column = 0 ; column < 16; column++) {
+function createGrid(gridSizeChoice) {
+    container.innerHTML = '';
+  
+    let boxSize = 500 / gridSizeChoice;
+  
+    for (let row = 0; row < gridSizeChoice; row++) {
+      const gridRow = document.createElement("div");
+      gridRow.classList.add('gridRow');
+      container.appendChild(gridRow);
+  
+      for (let column = 0; column < gridSizeChoice; column++) {
         const gridColumn = document.createElement("div");
+        gridColumn.classList.add('gridColumn');
+        gridColumn.style.width = `${boxSize}px`;
+        gridColumn.style.height = `${boxSize}px`;
+  
+        gridColumn.addEventListener('mouseenter', (event) => {
+          event.target.style.backgroundColor = 'black';
+        });
         gridRow.appendChild(gridColumn);
+      }
     }
+  }
+function promptForGridSize() {
+    let userGridChoice = parseInt(prompt("What size grid would you like? (1-100)"), 10);
+    while (isNaN(userGridChoice) || userGridChoice < 1 || userGridChoice > 100) {
+        userGridChoice = parseInt(prompt("Invalid input. Please enter a number between 1 and 100."), 10);
+    }
+    createGrid(userGridChoice);
 }
+
+const gridSizeButton = document.getElementById("grid-size-button")
+gridSizeButton.addEventListener('click', promptForGridSize);
+
+createGrid(16);
