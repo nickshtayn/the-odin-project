68d5b0900391f6c2131c9123404a83ea68f69e3a
Nick Shtayn <nickolasshtayn@protonmail.com>
2024-03-12 06:42:21 -0500

    Create pseudocode for project

---
+++ foundations/calculator/script.js
@@ -0,0 +1,30 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide(){
+
+}
+
+let firstNumber
+let operator
+let secondNumber
+
+function operate(firstNumber, operator, secondNumber) {
+    // calls one of the above functions on the numbers
+    // when user presses "=" key
+    // updates populateDisplay function with result
+}
+
+function populateDisplay() {
+    // displays the clicked buttons 
+    let displayValue
+}
